4c6a1c49692ee9460a17db0f75a4bb2de0382b60
CarlosCampos10 <53207200+CarlosCampos10@users.noreply.github.com>
2019-08-16 22:35:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -52,44 +52,13 @@ const showPage = (list, page) => {
    -- && the list item index is <= the index of the last item
    that should be shown on the page, show it
    */
-   }
+   
 
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-const appendPageLinks = (list) => {
-   showPage (list,page-1);
-   let pages = Math.ceil(list.lenght/ StudentsperPage);
-   const pagDiv = document.createElement('div');
-   pagDiv.className = 'pagination';
-   const divPage = document.querySelector('div.page');
-   divPage.appendChild(pagDiv);
-  
-  const ul = document.createElement('ul');
-  pagDiv.appendChild(ul);
-  
-  for (let i = 0; i  pages; i += 1) {
-    let li = document.createElement('li');
-    
-    let a = document.createElement('a');
-    a.textContent = i + 1;
-    a.href = '#';
-    
-    ul.appendChild(li);
-    li.appendChild(a);
-    
-    a.addEventListener(click, (e) => {
-      showPage(list, i +1)
-      e.target.className = 'active';
-    }
-                      );
-    
-    appendPageLinks(students);
-
-   
    /*
    1. Determine how many pages are needed for the list by dividing the
    total number of list items by the max number of items per page
@@ -102,10 +71,40 @@ const appendPageLinks = (list) => {
    7. Add the active class to the link that was just clicked. You can identify that
    clicked link using event.target
    */
-   }
+  const appendPageLinks = (list) => {
+   showPage (list,page-1);
+   let pages = Math.ceil(list.lenght/ StudentsperPage);
+   const pagDiv = document.createElement('div');
+   pagDiv.className = 'pagination';
+   const divPage = document.querySelector('div.page');
+   divPage.appendChild(pagDiv);
+  
+  const ul = document.createElement('ul');
+  pagDiv.appendChild(ul);
+  
+  for (let i = 0; i<list.length  += 1); 
+  {
+    let li = document.createElement('li');
+    
+    let a = document.createElement('a');
+    a.textContent = i + 1;
+    a.href = '#';
+  }
+    
+    ul.appendChild(li);
+    li.appendChild(a);
+    
+    a.addEventListener(click, (e) => {
+      showPage(list, i + 1)
+      e.target.className = 'active';
+    }
+                      );
+    
+    appendPageLinks(students);
+
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
 
 
-https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+//https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
