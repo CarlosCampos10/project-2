2c870340b49df9dcef1dfd1a8610c89e96aa8e35
CarlosCampos10 <53207200+CarlosCampos10@users.noreply.github.com>
2019-08-31 23:22:22 -0700

    Update script.js

--- Project 2/script.js
+++ Project 2/script.js
@@ -1,57 +1,66 @@
-const studentList = document.querySelectorAl('StudentsperPage');
-const itemsperPage = 10;
-var currentPage = 1;
+// my  variables i will work with the functions
+const studentList = document.querySelectorAll(".student-item");
+const itemsPerPage = 10;
+let currentPage = 1;
+let studentsToShow = studentList;
 
-
-function showPage(pageNumber, pageList) {
-    $("StudentsperPage").hide();
-    $.each(pageList, function (index, page){
-        if (pageNumber === index) {
-            $.each(page, function (i, listItem){
-                $(listItem).fadeIn;
-            })
-        }    
-    });
+//function to display pages with names of the students
+function showPage(list, page) {
+  let startIndex = page * itemsPerPage - itemsPerPage;
+  let endIndex = page * itemsPerPage;
+  //loop to show only 10 students on each page and hide the rest
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
 }
+//this function divides the total student number by capacity for each page (10)
+const appendPageLinks = list => {
+  const totalPages = Math.ceil(list.length / itemsPerPage);
+  //div created and named 'pagination' and added to '.page'
+  const div = document.createElement("div");
+  div.className = "pagination";
+  document.querySelector(".page").appendChild(div);
+  // ul tag added to the div
+  let ul = document.createElement("ul");
+  div.appendChild(ul);
 
-        if(i >= (page-1)*itemsPerPage && i < page*itemsPerPage) 
-          $(this).show();
-        else
-          $(this).hide();;
-    $("#currentPage").text(currentPage);
-    
-$(document).ready(function(){
-    showPage(currentPage);
-    $("#next").click(function(){
-       showPage(++currentPage);
-    });
-    $("#prev").click(function(){
-       showPage(--currentPage);
-    });
-});
-
-const appendPageLinks = () => {
-const div = document.querySelector('div.page');
-pagDiv.className = 'pagination';
-const ulPagination = document.createElement('ul');
-divPagination.appendChild(ulPagination);
-const maxPages = math.ceil(studentList.length / 10);
-
-let (i =0; i < maxPages; i++); {
-    let li = createElement('li');
-    ulPagination.appendChild(li);
+  //append for 'li' and 'a' tags
+  for (let i = 0; i < totalPages; i += 1) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setattribute = "href";
+    a.textContent = i + 1;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  //https://www.w3schools.com/jsref/met_node_appendchild.asp
+  //selects li tags(students) to display
+  let liLinks = document.querySelector("li");
+  let activeLinks = document.querySelectorAll("a");
+  //functionality
+  function removeClass() {
+    //loop to remove 'active' class from all class links
+    for (var i = 0; i < activeLinks.length; i++) {
+      activeLinks[i].classList.remove("active");
+      if (event.target) {
+        event.target = "";
+      }
+    }
+  }
+  //event lisnterner to display page clicked on
+  ul.addEventListener("click", e => {
+    showPage(list, event.target.textContent);
+    removeClass();
+  });
 };
-};
- const aList = ulPagination.querySelector('a');
- 
- divPagination.addEventListener('click', (e) => {
-     if (e.target.tagName === "a") {
-         let currentPage = e.target.textContent;
-         showPage(studentList, currentPage);
-         clearActiveclass(aList);
-         setActiveclass(e.target);
-     }
- })
+//Append to have buttons show
+showPage(studentList, 1);
+appendPageLinks(studentList);
+
 
 
 
